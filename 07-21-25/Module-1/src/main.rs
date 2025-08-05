c5d9ec31a50170f3e6ce55a1aca44ca24d37aa11
wolf031568 <a_mascorro@live.com>
2025-08-04 20:56:03 -0500

    update

--- 07-21-25/Module-1/src/main.rs
+++ 07-21-25/Module-1/src/main.rs
@@ -6,22 +6,30 @@ fn main() {
     guessing_game();
     println!("Program ended.");
 }
+
+//Assignment 1: Temperature Converter
 fn fahrenheit_to_celsius(f: f64) -> f64{
+    //Celsius -> Fahrenheit conversion formula: C = (F - 32) * (5/9)
     (f - 32.0) * (5.0/9.0)
 }
 
 fn celsius_to_fahrenheit(c: f64) -> f64{
+    //Fahrenheit -> Celsius conversion formula: F = (C * (9/5)) + 32
     (c * (9.0/5.0)) + 32.0
 }
 
 fn is_even(n: i32) -> bool {
+    //Returns true if number is even, false if odd
     n % 2 == 0
 }
 
+//Assignment 2: Number Analyzer
 fn number_analyzer(){
+    //Rand numbers for testing
     let nums: [i32; 10] = [28, 55, 79, 80, 99, 21, 34, 67, 90, 8];
-
+    //For loop to go through each of the numbers
     for &num in &nums {
+        //if-else statements to assign an output based on the number
         if num % 15 == 0 {
             println!("{}: FizzBuzz", num);
         } 
@@ -38,6 +46,7 @@ fn number_analyzer(){
             println!("{}: Odd", num);
         }
     }
+    //initialize the sum and index that will be used to stop the while loop
     let mut sum: i32 = 0;
     let mut i = 0;
     //while loop to find the sum of the numbers
@@ -56,12 +65,14 @@ fn number_analyzer(){
     println!("Largest number: {}", largest);
 }
 
+//Assignment 3: Guessing Game
 fn guessing_game(){
 let secret: i32 = 21;
 let mut attemps = 0;
 let guesses: [i32; 6] = [50, 10, 75, 20, 30, 21];
 for &guess in &guesses {
     attemps += 1;
+    //simple match statement to check each guess with the secret number.
     match check_guess(guess, secret){
         0 => {
             println!("Guess {} is correct. Attemps: {}", guess, attemps);
@@ -79,7 +90,7 @@ for &guess in &guesses {
     }
 }
 }
-
+//Checks the guess against the secret number (Separated for clarity)
 fn check_guess(guess : i32, secret : i32) -> i32 {
     if guess == secret {
         0
